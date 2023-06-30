118dd5acd9054ef71bae4bab8d8eb24eb69aa860
Albert Serrador <albertserrador>
2023-06-30 15:28:28 +0200

    Con audio de youtube

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,31 +1,66 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Viaje } from '../../interfaces/viaje.interface';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements AfterViewInit {
+export class FooterComponent implements OnInit {
 
   // private audio: HTMLAudioElement;
   anyo : number = new Date().getFullYear();
+
+  // audioUrl!: SafeResourceUrl;
+  // isPlaying: boolean = false;
+  // audioElement!: HTMLAudioElement;
+  
+
+  constructor(/*private sanitizer: DomSanitizer*/) {
+    
+  }
+
+
+  ngOnInit(): void {
+    // console.log('ngOnInit +audioMar.mp3');
+    // const audioSrc = '/assets/audios/audioMar.mp3';
+    // this.audioUrl = this.sanitizer.bypassSecurityTrustResourceUrl(audioSrc);
+    // this.audioElement = new Audio(audioSrc);
+    // // console.log(this.audioElement);
+    // this.audioElement.addEventListener('ended', () => {
+    //   this.isPlaying = false;
+    // });
+  }
+
+  // playAudio(): void {
+  //   // console.log('playAudio ');
+  //   this.audioElement.play();
+  //   this.isPlaying = true;
+  // }
+
+  // pauseAudio(): void {
+  //   // console.log('playAudio ');
+  //   this.audioElement.pause();
+  //   this.isPlaying = false;
+  // }
 
   // @ViewChild('audioPlayer') audioPlayerRef!: ElementRef<HTMLAudioElement>;
   // audioMar: HTMLAudioElement;
   
 
-  constructor() {
-    // this.audioMar = new Audio('/assets/audios/audioMar.mp3');  
-    //No deja hacerlo aquí, tenemos que hacerlo en el afterViewInit, para que pille el cambio del volumen por defecto
-    // this.audioMar.volume = 0.2;
-    // this.audioMar.play();    
-  }
+  // constructor(private sanitizer: DomSanitizer) {
+  //   this.audioMar = new Audio('/assets/audios/audioMar.mp3');  
+  //   //No deja hacerlo aquí, tenemos que hacerlo en el afterViewInit, para que pille el cambio del volumen por defecto
+  //   // this.audioMar.volume = 0.2;
+  //   // this.audioMar.play();    
+  // }
 
-  ngAfterViewInit() {
-    // const audioPlayer = this.audioPlayerRef.nativeElement;
-    // audioPlayer.oncanplaythrough = () => {
-    //   audioPlayer.volume = 0.2; // Establecer el volumen deseado
-    // };
-  }
+  // ngAfterViewInit() {
+  //   const audioPlayer = this.audioPlayerRef.nativeElement;
+  //   audioPlayer.oncanplaythrough = () => {
+  //     audioPlayer.volume = 0.2; // Establecer el volumen deseado
+  //   };
+  // }
 
 }
