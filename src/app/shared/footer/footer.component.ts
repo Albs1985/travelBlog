69d7084f8b984725604284688287afb98905d066
Albert Serrador <albertserrador>
2023-06-30 13:30:09 +0200

    Audio 3

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements AfterViewInit, OnInit{
+export class FooterComponent implements AfterViewInit {
 
   // private audio: HTMLAudioElement;
   anyo : number = new Date().getFullYear();
@@ -15,7 +15,7 @@ export class FooterComponent implements AfterViewInit, OnInit{
   
 
   constructor() {
-    // this.audioMar = new Audio();  
+    // this.audioMar = new Audio('/assets/audios/audioMar.mp3');  
     //No deja hacerlo aquí, tenemos que hacerlo en el afterViewInit, para que pille el cambio del volumen por defecto
     // this.audioMar.volume = 0.2;
     // this.audioMar.play();    
@@ -26,17 +26,6 @@ export class FooterComponent implements AfterViewInit, OnInit{
     // audioPlayer.oncanplaythrough = () => {
     //   audioPlayer.volume = 0.2; // Establecer el volumen deseado
     // };
-  }
-
-  playAudio(): void {
-    // this.audioMar.src = '/assets/audios/audioMar.mp3';
-    // this.audioMar.load();
-    // this.audioMar.play();    
-    // this.audioMar.loop = true;
-  }
-
-  ngOnInit(): void {
-    // this.playAudio();
   }
 
 }
