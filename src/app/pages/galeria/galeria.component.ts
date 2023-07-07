729a3d4275e6cff75730523e1acae0205dc82361
Albert Serrador <albertserrador>
2023-07-07 14:58:20 +0200

    Correciones

--- src/app/pages/galeria/galeria.component.ts
+++ src/app/pages/galeria/galeria.component.ts
@@ -14,11 +14,11 @@ export class GaleriaComponent implements OnInit{
   p: number = 1; // Página inicial
   pageSize: number = 10; // Elementos por página
   maxSizeDisplay: number = 5;
-  paginacionResponsive = true;//SI QUEREMOS QUE SEA RESPONSIVE AÑADIR ESTO COMO ATRIBUTO DE LA PAGINACION [responsive]="paginacionResponsive"
+  // paginacionResponsive = true;//SI QUEREMOS QUE SEA RESPONSIVE AÑADIR ESTO COMO ATRIBUTO DE LA PAGINACION [responsive]="paginacionResponsive"
 
   constructor(private route: ActivatedRoute, public viajerosService : ViajerosService, public viajesService : ViajesService){
     this.maxSizeDisplay = 5;
-    this.paginacionResponsive = true;
+    // this.paginacionResponsive = true;
   }
 
   ngOnInit(): void {
