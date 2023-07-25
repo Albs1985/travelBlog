7e9cef3f0dda278975dfe4b798651575b1392075
Albert Serrador <albertserrador>
2023-07-25 09:21:52 +0200

    correo contacto

--- src/app/pages/contact-qr/contact-qr.component.ts
+++ src/app/pages/contact-qr/contact-qr.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -21,76 +19,45 @@ export class ContactQRComponent {
   constructor(private translate: TranslateService) {    
   }
 
-  envia() {
-    // console.log('Envia correo');
-    // if($('#nombreTxt').val() == ''){
-    //   this.nom = '';
-    // } else {
-    //   this.nom = $('#nombreTxt').val();
-    //   // this.nom = this.nom.replace('!', ' ');
-    //   // this.nom = this.nom.replace(',', ' ');
-    //   // this.nom = this.nom.replace('.', ' ');
-    //   // this.nom = this.nom.replace(':', ' ');
-    //   // this.nom = this.nom.replace(';', ' ');
-    //   // this.nom = this.nom.replace('-', ' ');
-    //   // this.nom = this.nom.replace('_', ' ');
+  calculaCorreo() {
+    // console.log('calculaCorreo ');
+    if($('#nombreTxt').val() == ''){
+      this.nom = '';
+    } else {
+      this.nom = $('#nombreTxt').val();
+    }
 
-    // }
-
-    // if($('#emailTxt').val() == ''){
-    //   this.mail = '';
-    // } else {
-    //   this.mail = $('#emailTxt').val();
-    //   // this.mail = this.mail.replace('!', ' ');
-    //   // this.mail = this.mail.replace(',', ' ');
-    //   // this.mail = this.mail.replace('.', ' ');
-    //   // this.mail = this.mail.replace(':', ' ');
-    //   // this.mail = this.mail.replace(';', ' ');
-    //   // this.mail = this.mail.replace('-', ' ');
-    //   // this.mail = this.mail.replace('_', ' ');
-    // }
+    if($('#emailTxt').val() == ''){
+      this.mail = '';
+    } else {
+      this.mail = $('#emailTxt').val();
+    }
     
     if($('#mensajeTxt').val() == ''){
       this.mensajeCorreo = '';
     } else {
       this.mensajeCorreo = $('#mensajeTxt').val();
-      // this.mensajeCorreo = this.mensajeCorreo.replace('!', ' ').replace(',', ' ').replace('.', ' ').replace(':', ' ').replace(';', ' ').replace('-', ' ').replace('-', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace('!', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace(',', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace('.', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace(':', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace(';', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace('-', ' ');
-      // this.mensajeCorreo = this.mensajeCorreo.replace('_', ' ');
     }
 
-    // console.log(this.translate.currentLang);
     //Construimos el mensaje    
     this.correo = 'mailto:albertserrador@gmail.com?subject=';
     if (this.translate.currentLang == 'es'){
       this.correo = this.correo + this.asuntoES;
     }else{
       this.correo = this.correo + this.asuntoVAL;
-    }
-    
-    // this.correo = this.correo + '&cc='+$('#emailTxt').val();
-
+    }        
     if (this.mensajeCorreo != null && this.mensajeCorreo != ''){
       this.correo = this.correo +'&body='+this.mensajeCorreo;
     }
-    // if (this.nom != null && this.nom != ''){
-    //   this.correo = this.correo +'%0D%0A'+ this.nom;
-    // }
-    // if (this.mail != null && this.mail != ''){
-    //   this.correo = this.correo + ': '+ this.mail;
-    // }
+    if (this.nom != null && this.nom != ''){
+      this.correo = this.correo +'%0D%0A'+ this.nom;
+    }
+    if (this.mail != null && this.mail != ''){
+      this.correo = this.correo + '&cc='+ this.mail;
+    }
 
-    // console.log('nom: '+this.nom);
-    // console.log('mail: '+this.mail);
-    // console.log('mensajeCorreo: '+this.mensajeCorreo);
-    // console.log(this.correo);
-
+    console.log(this.correo);
+    
   }
-
 
 }
