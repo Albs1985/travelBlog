b3478065133258b524ee746c6b498ee60588ba73
Albert Serrador Sánchez <aserrado@mercadona.es>
2024-02-23 12:46:05 +0100

    Prueba para iPhone 24

--- src/app/pages/viajes/viajes.component.ts
+++ src/app/pages/viajes/viajes.component.ts
@@ -9,7 +9,8 @@ import { Viajes } from 'src/app/interfaces/viajes.interface';
   templateUrl: './viajes.component.html',
   styleUrls: ['./viajes.component.css']
 })
-export class ViajesComponent implements OnInit{
+export class ViajesComponent{
+  // implements OnInit
 
   orderDesc : boolean;
   ejecutarSoloTouch: boolean;
@@ -19,31 +20,31 @@ export class ViajesComponent implements OnInit{
     private route: ActivatedRoute,
     private translate: TranslateService){
 
-      this.preloadImages();
+      // this.preloadImages();
       this.orderDesc = true;
       this.ejecutarSoloTouch = false;
       this.servicioViajes.cargarViajes();
   }
-  ngOnInit(): void {
-    // Pre cargar imágenes
-    this.preloadImages();
-  }
+  // ngOnInit(): void {
+  //   // Pre cargar imágenes
+  //   // this.preloadImages();
+  // }
 
-  preloadImages(): void {
-    const imagesToPreload: string[] = [
-      'assets/images/2019/Portada2019.jpg',
-      'assets/images/2020/Portada2020.jpg',
-      'assets/images/2021/Portada2021.jpg',
-      'assets/images/2022/Portada2022.jpg',
-      'assets/images/2023/Portada2023.jpg',
-      'assets/images/2024/Portada2024.jpg'
-    ];
+  // preloadImages(): void {
+  //   const imagesToPreload: string[] = [
+  //     'assets/images/2019/Portada2019.jpg',
+  //     'assets/images/2020/Portada2020.jpg',
+  //     'assets/images/2021/Portada2021.jpg',
+  //     'assets/images/2022/Portada2022.jpg',
+  //     'assets/images/2023/Portada2023.jpg',
+  //     'assets/images/2024/Portada2024.jpg'
+  //   ];
 
-    imagesToPreload.forEach(imageUrl => {
-      const img = new Image();
-      img.src = imageUrl;
-    });
-  }
+  //   imagesToPreload.forEach(imageUrl => {
+  //     const img = new Image();
+  //     img.src = imageUrl;
+  //   });
+  // }
 
   public ordenar(eventType: string): void {
 
