0202f80c9f50ea8abe459cb969c2081188bee036
Albert Serrador Sánchez <aserrado@mercadona.es>
2024-03-12 12:32:43 +0100

    Indice de recetas

--- src/app/pages/recetas/recetas.component.ts
+++ src/app/pages/recetas/recetas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Receta } from 'src/app/interfaces/receta.interface';
 import { AutenticationService } from 'src/app/services/autentication.service';
 import { FrasesService } from 'src/app/services/frases.service';
 import { RecetasService } from 'src/app/services/recetas.service';
@@ -12,11 +13,26 @@ import { RecetasService } from 'src/app/services/recetas.service';
 })
 export class RecetasComponent {
 
-  p: number = 1; // Página inicial
-  pageSize: number = 1; // Elementos por página
-  maxSizeDisplay: number = 5;
+  // p: number = 1; // Página inicial
+  // pageSize: number = 1; // Elementos por página
+  // maxSizeDisplay: number = 5;
   palabra : any = '';
   categoria : any = '';
+  recetaMostrada : Receta = {
+    id: 0,
+    nombre: '',
+    categoria: '',
+    cocinero: '',
+    dificultad: '',
+    ingredientes: [],
+    utensilios: [],
+    tiempoPreparacion: 0,
+    tiempoHorno: 0,
+    temperatura: 0,
+    numPasos: [],
+    imagen: '',
+    raciones: 0
+  };
 
   constructor(private route: ActivatedRoute, private translate: TranslateService, public servicioRecetas: RecetasService, public authenticationService: AutenticationService){
     this.palabra = '';
@@ -24,8 +40,7 @@ export class RecetasComponent {
   }
 
   buscarRecetas(tipo: any){
-    this.p = 1; // Inicializo la página
-
+    // this.p = 1;
     this.categoria = tipo;
     if (tipo === ''){
       if($('#myInputReceta').val() == ''){
@@ -37,7 +52,31 @@ export class RecetasComponent {
       this.palabra = tipo;
     }
 
-    this.servicioRecetas.buscarRecetas(this.palabra);
+    this.servicioRecetas.buscarRecetas(this.palabra).then(()=>{
+
+      if (this.servicioRecetas.recetas.length === 1){
+        this.recetaMostrada = this.servicioRecetas.recetas[0];
+      }else{
+        this.recetaMostrada = {
+          id: 0,
+          nombre: '',
+          categoria: '',
+          cocinero: '',
+          dificultad: '',
+          ingredientes: [],
+          utensilios: [],
+          tiempoPreparacion: 0,
+          tiempoHorno: 0,
+          temperatura: 0,
+          numPasos: [],
+          imagen: '',
+          raciones: 0
+        };
+      }
+
+      // console.log(this.recetaMostrada);
+    });
+
     this.palabra = '';
   }
 
